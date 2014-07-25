8d97e377bdc50540c2e4298e04586c5555f42d80
Vols <zhengcan@keegoo.com>
2014-07-25 12:29:12 +0800

    no message

--- ColorPickerView/ColorPickerView/ViewController.m
+++ ColorPickerView/ColorPickerView/ViewController.m
@@ -10,104 +10,98 @@
 #import "ColorPickerView.h"
 #import "UIColor+extensions.h"
 
-@interface ViewController () <ColorPickerViewDelegate>{
-    NSInteger gridSize;
+@interface ViewController () <ColorPickerViewDelegate> {
+  NSInteger gridSize;
 }
 
-@property (nonatomic, strong) ColorPickerView * colorPickerView;
-
+@property(nonatomic, strong) ColorPickerView *colorPickerView;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    
-    self.navigationController.navigationBar.translucent = NO;
-    [self.view addSubview:self.colorPickerView];
-    [self.colorPickerView setColors:[self createDemoColor]];
-    
-    gridSize = 4;
-    
-    UIBarButtonItem *changeGrid = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemRefresh target:self action:@selector(changeGridSize)];
-    changeGrid.tintColor = [UIColor blackColor];
-    
-    self.navigationItem.rightBarButtonItem = changeGrid;
+- (void)viewDidLoad {
+  [super viewDidLoad];
+
+  self.navigationController.navigationBar.translucent = NO;
+  [self.view addSubview:self.colorPickerView];
+  [self.colorPickerView setColors:[self createDemoColor]];
+
+  gridSize = 4;
+
+  UIBarButtonItem *changeGrid = [[UIBarButtonItem alloc]
+      initWithBarButtonSystemItem:UIBarButtonSystemItemRefresh
+                           target:self
+                           action:@selector(changeGridSize)];
+  changeGrid.tintColor = [UIColor blackColor];
+
+  self.navigationItem.rightBarButtonItem = changeGrid;
 }
 
--(void)changeGridSize
-{
-    gridSize++;
-    
-    if (gridSize > 10) {
-        gridSize = 2;
-    }
-    
-    _colorPickerView.colorsPerRow = gridSize;
+- (void)changeGridSize {
+  gridSize++;
+
+  if (gridSize > 10) {
+    gridSize = 2;
+  }
+
+  _colorPickerView.colorsPerRow = gridSize;
 }
 
+- (void)viewWillLayoutSubviews {
+  [super viewWillLayoutSubviews];
 
--(void)viewWillLayoutSubviews
-{
-    [super viewWillLayoutSubviews];
-    
-    _colorPickerView.frame = self.view.bounds;
+  _colorPickerView.frame = self.view.bounds;
 }
 
--(void)colorPickerView:(ColorPickerView *)colorPickerView didSelectedColor:(UIColor *)color
-{
-    self.navigationController.navigationBar.barTintColor = color;
+- (void)colorPickerView:(ColorPickerView *)colorPickerView
+       didSelectedColor:(UIColor *)color {
+  self.navigationController.navigationBar.barTintColor = color;
 }
 
+- (NSArray *)createDemoColor {
+  NSMutableArray *colors = [[NSMutableArray alloc] init];
 
+  NSInteger colorCount = 36;
+  NSInteger createdColors = 0;
+  CGFloat hueStep = 1.0 / colorCount;
 
--(NSArray *)createDemoColor
-{
-    NSMutableArray *colors = [[NSMutableArray alloc] init];
-    
-    NSInteger colorCount = 36;
-    NSInteger createdColors = 0;
-    CGFloat hueStep = 1.0/colorCount;
-    
-    CGFloat currentHue = 0.0;
-    
-    while (createdColors < colorCount) {
-        
-        UIColor *tmpColor = [UIColor colorWithHue:currentHue saturation:1.0 brightness:1.0 alpha:1.0];
-        [colors addObject:tmpColor];
-        currentHue+=hueStep;
-        
-        createdColors++;
-    }
-    
-    return colors;
+  CGFloat currentHue = 0.0;
+
+  while (createdColors < colorCount) {
+
+    UIColor *tmpColor = [UIColor colorWithHue:currentHue
+                                   saturation:1.0
+                                   brightness:1.0
+                                        alpha:1.0];
+    [colors addObject:tmpColor];
+    currentHue += hueStep;
+
+    createdColors++;
+  }
+
+  return colors;
 }
 
+- (ColorPickerView *)colorPickerView {
+  if (!_colorPickerView) {
+    _colorPickerView = [[ColorPickerView alloc] init];
 
+    _colorPickerView.delegate = self;
 
-- (ColorPickerView *)colorPickerView{
-    if (!_colorPickerView) {
-        _colorPickerView = [[ColorPickerView alloc] init];
-        
-        _colorPickerView.delegate = self;
-        
-        _colorPickerView.colorsPerRow = 4;
-        _colorPickerView.colorCellPadding = 2.0;
-        
-        _colorPickerView.highlighting = YES;
-        _colorPickerView.selectionBorderColor = [UIColor whiteColor];
-        _colorPickerView.backgroundColor = [UIColor colorWithHexString:@"1d2225"];
+    _colorPickerView.colorsPerRow = 4;
+    _colorPickerView.colorCellPadding = 2.0;
 
-    }
-    return _colorPickerView;
+    _colorPickerView.highlighting = YES;
+    _colorPickerView.selectionBorderColor = [UIColor whiteColor];
+    _colorPickerView.backgroundColor = [UIColor colorWithHexString:@"1d2225"];
+  }
+  return _colorPickerView;
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
 }
 
 @end
